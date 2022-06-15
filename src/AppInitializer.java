947d01dc9fb48264fa7e0a4c1d8631d5df67d689
RavinduAX <ravinduadassuriya59@gmail.com>
2022-06-16 04:33:33 +0530

    Setup xml configuration

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,3 +1,17 @@
-public class AppInitializer {
+import lk.ijse.thogakade.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+public class AppInitializer {
+    public static void main(String[] args) {
+
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        transaction.commit();
+        session.close();
+
+    }
 }
